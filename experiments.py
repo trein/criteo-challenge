e76dcbb8eb30f5eb6b3e38d5123989a74b51a422
Guilherme M. Trein <guitrein@yahoo.com.br>
2014-09-09 21:19:39 -0400

    Check for data normalization

--- experiments.py
+++ experiments.py
@@ -1,23 +1,56 @@
 import constants
 import csv
+import gzip
 
-# balanced data?
-with open(constants.TRAIN_EXPANDED, 'r') as source:
-    reader = csv.reader(source)
-    next(reader, None)
 
-    n_samples = 0
-    with_click = 0
+def balanced():
+    # balanced data?
+    with gzip.open(constants.TRAIN_EXPANDED, 'r') as source:
+        reader = csv.reader(source)
+        next(reader, None)
 
-    for feature_vector in reader:
-        click = feature_vector[1]
+        n_samples = 0
+        with_click = 0
 
-        ad_id = feature_vector[0]
-        features = feature_vector[2:]
+        for feature_vector in reader:
+            click = feature_vector[1]
 
-        with_click = with_click + 1 if click == 1 else with_click
-        n_samples += 1
+            ad_id = feature_vector[0]
+            features = feature_vector[2:]
 
-    print 'Experiments results'
-    print '-------------------'
-    print 'Clicks [%s] out of [%s] - [%s] with click' % (with_click, n_samples, float(with_click) / n_samples)
+            with_click = with_click + 1 if click == 1 else with_click
+            n_samples += 1
+
+        print 'Experiments results'
+        print '-------------------'
+        print 'Clicks [%s] out of [%s] - [%s] with click' % (with_click, n_samples, float(with_click) / n_samples)
+
+
+def normalized():
+    with gzip.open(constants.TRAIN_EXPANDED, 'r') as source:
+        reader = csv.reader(source)
+        next(reader, None)
+
+        n_samples = 0
+        with_click = 0
+
+        for feature_vector in reader:
+            click = feature_vector[1]
+
+            ad_id = feature_vector[0]
+            features = feature_vector[2:]
+
+            for f in features:
+                if float(f) > 1.0:
+                    print 'Not normalized features'
+                    print features
+                    break
+
+        # print 'Experiments results'
+        # print '-------------------'
+        # print 'Clicks [%s] out of [%s] - [%s] with click' % (with_click, n_samples, float(with_click) / n_samples)
+
+
+if __name__ == '__main__':
+    # balanced()
+    normalized()
